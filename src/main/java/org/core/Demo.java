8f1ffa3f29f30d7a8823b8fb790025a05b294721
Nitu Yadav <126680562+Nituyadav0109@users.noreply.github.com>
2026-02-10 14:08:19 +0530

    Clean up Demo.java by removing commented code
    Removed commented-out code for clarity.

--- src/main/java/org/core/Demo.java
+++ src/main/java/org/core/Demo.java
@@ -1,35 +1,4 @@
 
-// package org.core;
-
-// import java.util.*;
-
-// public class Demo {
-
-//     public static void main(String[] args) {
-
-//         Map<Integer, List<Integer>> graph = new HashMap<>();
-//         graph.put(1, List.of(2, 3, 4));
-//         graph.put(2, List.of(3, 5));
-//         graph.put(3, List.of(5));
-//         graph.put(4, List.of(5));
-//         graph.put(5, List.of());
-
-//         PrivacyPreservingGraph pg = new PrivacyPreservingGraph(graph);
-
-//         Scanner scanner = new Scanner(System.in);
-//         while (true) {
-//             System.out.print("Enter node to query degree (-1 to exit): ");
-//             int node = scanner.nextInt();
-//             if (node == -1) break;
-//             System.out.println("Privacy-safe degree = " + pg.degree(node));
-//         }
-
-//         System.out.println("Average Degree = " +
-//                 AnalyticsService.averageDegree(pg));
-//         scanner.close();
-//     }
-// }
-
 package org.core;
 
 import java.util.*;
